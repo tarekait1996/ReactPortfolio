0fc6f9cbfd3c8a7ed626b9f75332c60a84cb67ba
Tarek Ait Hamouda <tarekaithamouda@gmail.com>
2019-10-21 01:02:41 -0400

    refactoring of infolayout page

--- src/components/LandingLayout/InformationLayout/InformationLayout.js
+++ src/components/LandingLayout/InformationLayout/InformationLayout.js
@@ -4,13 +4,14 @@ import Typist from "react-typist";
 import { Link, animateScroll as scroll } from "react-scroll";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 
 export default class InformationLayout extends React.Component {
   render() {
     return (
       <div id="main" className="container">
-        <div className = "tintLayer1"></div>
+        <div className="tintLayer1"></div>
         <div className="centeredContainer">
           <h1 className="title">
             <Typist blink="true">
@@ -18,16 +19,18 @@ export default class InformationLayout extends React.Component {
               <br />
               <span>I am Tarek Ait Hamouda.</span>
               <Typist.Backspace count={23} delay={2000} />
-              <span>Nice to meet you</span>
+              <span>Nice to meet you! </span>
             </Typist>
           </h1>
           <div className="description-Container">
             <p className="description">
-              Software Engineering student.
+              I am a
+              <br/>
+              Software Engineering Student,
               <br />
-              Mobile & web development enthusiast.
+              a Mobile & Web Development enthusiast,
               <br />
-              Love Art, Food and Music.
+              and I love Art, Food and Music.
             </p>
           </div>
           <hr />
@@ -37,6 +40,9 @@ export default class InformationLayout extends React.Component {
             </a>
             <a href="https://www.linkedin.com/in/tarek-ait-hamouda/">
               <FontAwesomeIcon className="faIcon" icon={faLinkedinIn} />
+            </a>
+            <a href="https://www.instagram.com/tarekait96/">
+              <FontAwesomeIcon className="faIcon" icon={faInstagram} />
             </a>
           </div>
         </div>
@@ -49,14 +55,14 @@ export default class InformationLayout extends React.Component {
           duration={500}
           onSetActive={this.handleSetActive}
         >
-          <div className="icon-scroll">
-            <div className="mouse">
-              <div className="wheel"></div>
+            <div className="icon-scroll">
+              <div className="mouse">
+                <div className="wheel"></div>
+              </div>
+              <div className="icon-arrows">
+                <span></span>
+              </div>
             </div>
-            <div className="icon-arrows">
-              <span></span>
-            </div>
-          </div>
         </Link>
       </div>
     );
