5b3542299a121f7cf964ed8aec9f2232372a82c7
Tarek Ait Hamouda <tarekaithamouda@gmail.com>
2020-01-05 15:42:46 -0500

    removed files other than build

---
+++ build/precache-manifest.b283fbaf7c02a5304a586076bef84a56.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4c738034c2acf5a883bd7d5fedc4e8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fdf8eec31cbc3f0faa4",
+    "url": "/static/css/main.65b4e45b.chunk.css"
+  },
+  {
+    "revision": "096e09b21571ab56a435",
+    "url": "/static/js/2.55fa3eb3.chunk.js"
+  },
+  {
+    "revision": "3fdf8eec31cbc3f0faa4",
+    "url": "/static/js/main.50420049.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7cf5a0b8c1592b5cd6d1bf11f9f736cd",
+    "url": "/static/media/background.7cf5a0b8.jpg"
+  },
+  {
+    "revision": "4bbf87d926cc0af147c998a350cf5984",
+    "url": "/static/media/devblockLogo.4bbf87d9.png"
+  },
+  {
+    "revision": "0309ef0a47265a17ec173722954e6350",
+    "url": "/static/media/htnPortfolio.0309ef0a.png"
+  },
+  {
+    "revision": "16b62e086619592bc3639325e08c0bd1",
+    "url": "/static/media/resume.16b62e08.pdf"
+  },
+  {
+    "revision": "7934b4b0d12d2e583384a086753f3544",
+    "url": "/static/media/twatterLogo.7934b4b0.png"
+  }
+]);
